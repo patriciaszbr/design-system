fe8db204be7811ff0ecc24309c08c57b6ed14646
Patrícia Souza <patricia.sz.br@gmail.com>
2023-04-25 11:37:21 -0300

    Configuração do Stitches

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,4 +1,12 @@
+import { styled } from './styles'
+
 import { colors } from '@tricia-ui/tokens'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite500',
+  borderRadius: '$md'
+}
 
 export function App() {
   return <h1 style={{ color: colors.ignite300 }}>Hello World</h1>
---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,37 @@
+import { createStitches, defaultThemeMap } from '@stitches/react'
+import {
+  colors,
+  fontSizes,
+  fontWeights,
+  fonts,
+  lineHeights,
+  radii,
+  space,
+} from '@tricia-ui/tokens'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  createTheme,
+  theme,
+  config,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    height: 'space',
+    width: 'space',
+  },
+
+  theme: {
+    colors,
+    fontSizes,
+    fontWeights,
+    fonts,
+    lineHeights,
+    radii,
+    space,
+  },
+})
