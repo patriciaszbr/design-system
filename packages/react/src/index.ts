da84245a29e92fa8c733a83aef8653ab5b03cb3c
Patrícia Souza <patricia.sz.br@gmail.com>
2023-04-25 11:13:35 -0300

    Configurando pacote do react

--- packages/react/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { colors } from '@tricia-ui/tokens'
-
-console.log(colors)
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,5 @@
+import { colors } from '@tricia-ui/tokens'
+
+export function App() {
+  return <h1 style={{ color: colors.ignite300 }}>Hello World</h1>
+}
